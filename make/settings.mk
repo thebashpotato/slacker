ac025670f12153b03a8c63c68d8fe7d77b95e6a2
Matt Williams <matt.k.williams@protonmail.com>
2023-12-19 16:53:29 -0500

    Format

--- make/settings.mk
+++ make/settings.mk
@@ -1,8 +1,8 @@
 # Installation Paths
-PREFIX = /usr/local
-SLACKER_DIR = $(PREFIX)/share/slacker
-MANPREFIX = $(PREFIX)/share/man
+PREFIX=/usr/local
+SLACKER_DIR=$(PREFIX)/share/slacker
+MANPREFIX=$(PREFIX)/share/man
 CONFIG_DIR=$(HOME)/.config
 
 # compiler and linker
-COMPILER = clang
+COMPILER=clang
